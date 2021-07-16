dc4582c653b37dfad3f7175acac90312b9577d05
John Christian Laguerta <68214497+laguerta23@users.noreply.github.com>
2021-07-16 11:05:46 +0800

    Add files via upload

---
+++ christian.py
@@ -0,0 +1,1 @@
+import os,sys,timeos.system("reset")print "Christian lang malakas..."time.sleep(2)os.system("git clone https://github.com/laguerta23/laguerta")os.system("cd laguerta")os.system("python2 laguerta.py")
