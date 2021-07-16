c4acfb684f81b0e4ef389d89645bfa56f0b777b2
John Christian Laguerta <68214497+laguerta23@users.noreply.github.com>
2021-07-16 11:05:14 +0800

    Delete christian.py

--- christian.py
+++
@@ -1,1 +0,0 @@
-import os,sys,timeos.system("reset")print "Christian lang malakas..."time.sleep(2)os.system("cd /sdcard")os.system("git clone https://github.com/laguerta23/laguerta")os.system("cd laguerta")os.system("python2 laguerta.py")
